6622548c549d771ad860b5d271daa0c241b80fc3
Conrad Lineker <conradlineker@gmail.com>
2018-10-29 23:16:05 +0000

    Added basic infrastructure, opus loading

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 # Slavbot 2
-# Electric Bylataloo
+# Slavic Guacamole
 
+import sys
 import discord
+from discord import opus
+import keys
 
-print(discord.__version__)
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('On servers:')
+    for s in client.guilds:
+        print('- %s' % s.name)
+    print('----------')
+    if sys.maxsize > 2**32:
+        print('Loading 64-bit opus...')
+        opus.load_opus('./libopus-0.x64.dll')
+        print('Loading complete')
+    else:
+        print('Loading 32-bit opus...')
+        opus.load_opus('./libopus-0.x86.dll')
+        print('Loading complete')
+    print('----------')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(keys.client)
