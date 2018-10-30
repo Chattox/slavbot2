df738a8d4a74ffb37fbbf126ec39704e313edec8
Conrad Lineker <conradlineker@gmail.com>
2018-10-30 12:43:21 +0000

    Added opus lib and dll, now loads correctly. Also added command parsing

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import sys
 import discord
 from discord import opus
 import keys
+#import slavsound
 import logging
 
+# Create logger, output log to file
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='slavbot.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 client = discord.Client()
 
+# Start up, load opus version based on 32/64bit
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -34,13 +37,19 @@ async def on_ready():
         print('Loading complete')
     print('----------')
 
-
+# Listen for commands
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(msg):
+    # If message is from slavbot, ignore
+    if msg.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    if msg.content.startswith("!"):
+        # Figure out what command it is and print to stdout, also convert to lowercase for case-insensitivity
+        cmd = msg.content[1:].split(" ")[0]
+        args = msg.content.split(" ")[1:]
+        cmd = cmd.lower()
+        print('COMMAND: %s' % (cmd))
+        print('FROM: ' + msg.author.name)
 
 client.run(keys.client)
